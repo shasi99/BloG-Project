cef8d1c44dd1ec275007702e1126cfcb52bfd721
shasi99 <silu.sietdkl@gmail.com>
2021-03-11 12:49:11 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  purge: {
+    
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brandcolor: '#2E7CA1',
+        branddark: '#01608c',
+        headercolor: '#596bf3',
+        footercolor:'#1b749e',
+      },
+      fontFamily:{
+        body: ['Open Sans']
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
